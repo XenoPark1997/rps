2133630db5bba80d308e4c92b78a38371ab9df51
XenoPark1997 <XenoPark1997@users.noreply.github.com>
2021-01-13 11:40:47 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './App.css';
+import img_style from './Component/assets/icons/rock.svg';
+class Rps extends Component {
+  render(){
+    return (
+        <div className="Rps_container">
+            <div className="Rps_box">
+              <div className="Player_box">
+                <label className="Top_label">YOU</label><br/>
+                <label className="Top_label">0</label>
+              </div>
+              <div className="Opponent_box">
+                <label className="Top_label" value="AI" /><br />
+                <label className="Top_label" value="0" />
+              </div>
+              <div className="Item_box">
+                <button className="rock_button" name="rock" value="">
+                  <img className="img_style" src="/Component/assets/icons/rock.svg" alt="meteor" />
+                </button>
+              </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default Rps;
