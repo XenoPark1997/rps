aa7074f72fb841471b2176a2c1a6e47add3d3c16
XenoPark1997 <XenoPark1997@users.noreply.github.com>
2021-01-13 12:57:16 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
+import rock from './Component/assets/icons/rock.svg';
+import paper from './Component/assets/icons/paper.svg';
+import scissor from './Component/assets/icons/scissors.svg';
+
 class Rps extends Component {
   render(){
     return (
@@ -7,21 +11,31 @@ class Rps extends Component {
             <div className="Rps_box">
               <div className="Player_box">
                 <label className="Top_label">YOU</label><br/>
-                <label className="Top_label">0</label>
+                <label className="Top_label" name="Player_Score">0</label>
               </div>
               <div className="Opponent_box">
-                <label className="Top_label" value="AI" /><br />
-                <label className="Top_label" value="0" />
+                <label className="Top_label" >AI</label><br />
+                <label className="Top_label" name="AI_Score">0</label>
               </div>
-              <label> meow </label>
+              <label className="scoreboard">SCOREBOARD</label>
+              <label className="Indicator" name="result">CHOOSE YOUR ITEM</label>  {/* Win Or lose Indicator text */}
               <div className="Item_box">
                 <button className="rock_button" name="rock" value="">
-                  <img className="img_style" src="/Component/assets/icons/rock.svg" alt="meteor" />
+                  <img className="img_style" src={rock} alt="meteor" />
+                  ROCK
+                </button>
+                <button className="paper_button" name="rock" value="">
+                  <img className="img_style" src={paper} alt="paper" />
+                  PAPER
+                </button>
+                <button className="scissor_button" name="rock" value="">
+                  <img className="img_style" src={scissor} alt="scissor" />
+                  SCISSOR
                 </button>
               </div>
+              <button className="proceed" name="proceed" value="PROCEED">PROCEED</button> {/* Button for Proceed and Try Again */}
             </div>
         </div>
-        
     );
   }
 }
---
+++ src/proceed.js
@@ -0,0 +1,7 @@
+impot react{Component} from 'react';
+
+function proceed{
+    render(){
+        
+    }
+}
